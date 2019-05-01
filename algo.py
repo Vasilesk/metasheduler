4df563447a1dbe039ffda8fa11fa46f62190c4f3
vasilesk <i@vasilesk.ru>
2019-05-01 20:15:45 +0300

    main algo step function

--- algo.py
+++ algo.py
@@ -11,7 +11,7 @@ def scheduler_exec(data):
     f_prog = open("tmp/out_prog","wb")
 
     exec_path = "local_algo/yulia_algo"
-    # exec_path = "local_algo/andrei_algo"
+    # exec_path = "local_algo/true_andrei_algo"
 
     call_data = [
         exec_path,
@@ -33,70 +33,88 @@ def main_algo(
         e,
         strategy_choose_tenant,
         strategy_choose_dc
-    ):
+):
     for tenant in tenants:
         tenant.evaluation = e.get_tenant_evaluation(tenant)
 
     dict_dcs = {dc.name : dc for dc in dcs}
     dict_tenants = {tenant.name : tenant for tenant in tenants}
 
+
     tenants_processed = [x for x in tenants if x.mark is None]
-
     while len(tenants_processed) > 0 and iter_max > 0:
+        algo_step(
+            dcs_per_tenant,
+            dcs,
+            tenants_processed,
+            e,
+            strategy_choose_tenant,
+            strategy_choose_dc,
+            dict_dcs,
+            dict_tenants
+        )
+
         iter_max -= 1
-
-        tenant_placements = dict()
-
-        for dc in dcs:
-            dc.evaluation = e.get_dc_evaluation(dc)
-
-        tenants_prior = strategy_choose_tenant(tenants_processed)
-        for tenant in tenants_prior:
-            dcs_count = 0
-            for dc in strategy_choose_dc(tenant, dcs):
-                if tenant.name in dc.names_rejected:
-                    continue
-                evaluation, placement_possible = e.place(dc, tenant)
-                if placement_possible:
-                    dc.evaluation = evaluation
-                    dc.tenants_try.append(tenant)
-
-                    dcs_count += 1
-                    if dcs_count == dcs_per_tenant:
-                        break
-            if dcs_count != 0:
-                tenant_placements[tenant.name] = []
-
-
-        exec_input = [x.pre_exec() for x in dcs]
-        with Pool(8) as p:
-            filenames_out = p.map(scheduler_exec, exec_input)
-        for dc, filename in zip(dcs, filenames_out):
-            placements = dc.after_exec(filename)
-            for placement, removings in placements:
-                tenant_placements[placement["name"]].append((dc.name, placement, removings))
-
-        # for the next step
-        tenants_processed = []
-
-        # choose dc for tenant
-        for name in tenant_placements:
-            tenant = dict_tenants[name]
-            placement_chosen = choose_placement(
-                tenant,
-                tenant_placements[name],
-                dict_dcs,
-                dict_tenants,
-                strategy_choose_dc,
-                e,
-                strategy_choose_tenant
-            )
-
-            if not placement_chosen:
-                tenants_processed.append(tenant)
+        tenants_processed = [x for x in tenants if x.mark is None]
 
     everything_placed = all([x.mark is not None for x in tenants])
     return iter_max, everything_placed
+
+def algo_step(
+    dcs_per_tenant,
+    dcs,
+    tenants,
+    e,
+    strategy_choose_tenant,
+    strategy_choose_dc,
+    dict_dcs,
+    dict_tenants
+):
+    tenant_placements = dict()
+
+    for dc in dcs:
+        dc.evaluation = e.get_dc_evaluation(dc)
+
+    tenants_prior = strategy_choose_tenant(tenants)
+    for tenant in tenants_prior:
+        dcs_count = 0
+        for dc in strategy_choose_dc(tenant, dcs):
+            if tenant.name in dc.names_rejected:
+                continue
+            evaluation, placement_possible = e.place(dc, tenant)
+            if placement_possible:
+                dc.evaluation = evaluation
+                dc.tenants_try.append(tenant)
+
+                dcs_count += 1
+                if dcs_count == dcs_per_tenant:
+                    break
+        if dcs_count != 0:
+            tenant_placements[tenant.name] = []
+
+
+    exec_input = [x.pre_exec() for x in dcs]
+    with Pool(8) as p:
+        filenames_out = p.map(scheduler_exec, exec_input)
+    for dc, filename in zip(dcs, filenames_out):
+        placements = dc.after_exec(filename)
+        for placement, removings in placements:
+            tenant_placements[placement["name"]].append((dc.name, placement, removings))
+
+    # choose dc for tenant
+    for name in tenant_placements:
+        tenant = dict_tenants[name]
+        choose_placement(
+            tenant,
+            tenant_placements[name],
+            dict_dcs,
+            dict_tenants,
+            strategy_choose_dc,
+            e,
+            strategy_choose_tenant
+        )
+
+    return all([x.mark is not None for x in tenants])
 
 def choose_placement(tenant, possible_placements, dict_dcs, dict_tenants, strategy_choose_dc, e, strategy_choose_tenant):
     if len(possible_placements) == 0:
@@ -147,14 +165,15 @@ def choose_placement(tenant, possible_placements, dict_dcs, dict_tenants, strate
 
         delete_placements(tenants_condition, dict_dcs, e)
 
-        _, placed_ok = main_algo(
-            1,
+        placed_ok = algo_step(
             len(other_dcs),
             other_dcs,
             tenants_condition,
             e,
             strategy_choose_tenant,
-            strategy_choose_dc
+            strategy_choose_dc,
+            dict_dcs,
+            dict_tenants
         )
 
         if placed_ok:
